de51ac92ce1c309c140f7197cac8e24c7897c054
Jas Johal <22663194+JSJohal@users.noreply.github.com>
2024-08-20 15:31:51 +0100

    added sign in folder

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -7,6 +7,18 @@ var school = 'school/'
 
 module.exports = router => {
     router.post(v + school + 'start', (req, res) => {
-        res.redirect(v + school + 'sign-in')
+        res.redirect(v + school + 'dfe-sign-in')
+    })
+
+    router.post(v + school + 'dfe-sign-in', (req, res) => {
+        res.redirect(v + school + 'need-to-know')
+    })
+
+    router.post(v + school + 'need-to-know', (req, res) => {
+        res.redirect(v + school + 'find-ect')
+    })
+
+    router.post(v + school + 'find-ect', (req, res) => {
+        res.redirect(v + school + 'review-ect-details')
     })
 }
